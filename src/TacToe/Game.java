ddc475987915afa13554ead367b1d600071efba0
vitaliy <andreyev.vitaliy@gmail.com>
2023-02-28 22:40:11 +0100

    TacToe

--- src/TacToe/Game.java
+++ src/TacToe/Game.java
@@ -13,8 +13,10 @@ import java.awt.event.ActionListener;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.Arrays;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.RepaintManager;
@@ -24,90 +26,89 @@ public class Game {
     public int pole[][] = new int[3][3];
     public int victory[] = {-1, -1, -1};
 
-    public JFrame frame;
-    public JLabel lblText;
-   
-    
+    private JFrame frame;
+    private JLabel lblText;
+    private JButton btnClear;
+    private JButton btnClear1;
+
     /**
-     * Принимает значения:
-     * 1 - нолики, -1 - крестики.
-     * 
+     * Принимает значения: 1 - нолики, -1 - крестики.
+     *
      * Переменная необходима для изменения игрока во время игры
      */
     public int player = 1; //принимает значения 1 - нолики, -1 крестики. Необходимо
 
-    Game() {
-        JFrame frame = new JFrame("MyGame");
-        JPanel panel = new JPanel();
-        lblText = new JLabel("Test");
-
+    public Game() {
+        frame = new JFrame("MyGame");
         frame.setSize(600, 700);
 
-        MouseListener mouseListener = new MouseListener();
-        panel.addMouseListener(mouseListener);
+        lblText = new JLabel();
 
-        
-        
-        Button btnClear = new Button("Очистить");
-        
         clearGamePole();
 
+        btnClear = new JButton("Очистить");
+        btnClear1 = new JButton("Очистить2");
         
-        //panel.add(btnClear);
-
-        btnClear.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.out.println(1);
-                clearGamePole();
-                
-                frame.repaint();
-
-            }
-        });
-
-
+        
+        ButtonListener btnListener = new ButtonListener();
+        btnClear.addActionListener(btnListener);
+        btnClear1.addActionListener(btnListener);
+        
 
         lblText.setBounds(200, 600, 300, 50);
         btnClear.setBounds(50, 600, 120, 50);
+        btnClear1.setBounds(250, 600, 120, 50);
+
 
         frame.add(btnClear);
+        frame.add(btnClear1);
         frame.add(lblText);
-
-        //frame.add(new DrawingLines());
         frame.add(new DrawCircleOnClick());
-        //frame.add(panel);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.pack();
+
         frame.setVisible(true);
 
     }
-    
+
+    public class ButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            
+            if (e.getSource() == btnClear) { //Нажатие на кнопку 
+                System.out.println(1);
+                clearGamePole();
+                frame.repaint();
+            } else {
+                System.out.println("Другая кнопка");
+            }
+
+        }
+    }
+
     /**
-     * Метод необходим для очистки результатов игры и подготовки 
-     * поля к новой игре
+     * Метод необходим для очистки результатов игры и подготовки поля к новой
+     * игре
      */
     public void clearGamePole() {
-        
+
         //Очищаем игровое поле
         for (int i = 0; i <= pole.length - 1; i++) {
             for (int j = 0; j <= pole[i].length - 1; j++) {
                 pole[i][j] = 0;
             }
         }
-        
+
         //восстанавливаем игрока
         player = 1;
-        
+
         //Очищаем информацию о победе
         victory[0] = -1;
         victory[1] = -1;
         victory[2] = -1;
-        
-        
-        
+
         //Восстанавливаем форму
         //frame.repaint();
-    
     }
 
     public class DrawCircleOnClick extends JPanel {
@@ -122,16 +123,12 @@ public class Game {
                     x = identifyCell(e.getX());
                     y = identifyCell(e.getY());
 
-                    pole[y][x] = player;
-
-                    //showArray(pole);
+                    pole[y][x] = player; //  записываем значение игрока в массив
                     player *= -1; //меняем игрока при каждом нажатии
-                    
-                    lblText.setText("11223");
-                    gameIsEnd();
+
+                    isEnd();
                     repaint();
                     victoryMessage();
-                    
 
                 }
             });
@@ -140,20 +137,24 @@ public class Game {
         private void victoryMessage() {
 
             if (victory[0] > -1) {
-                JOptionPane.showMessageDialog(null, "Выиграли " + ((victory[2] < -2) ? "крестики" : "нолики"));
+                JOptionPane.showMessageDialog(frame, "Выиграли " + ((victory[2] < -2) ? "крестики" : "нолики"));
+                lblText.setText("Выиграли " + ((victory[2] < -2) ? "крестики" : "нолики"));
                 clearGamePole();
             }
 
         }
 
-        private void gameIsEnd() {
+        /**
+         * Метод определяет исходя из состояния массива закончена ли игра или
+         * нет
+         */
+        private void isEnd() {
 
             for (int i = 0; i <= pole.length - 1; i++) {
                 int tempSumX = pole[i][0] + pole[i][1] + pole[i][2];
                 int tempSumY = pole[0][i] + pole[1][i] + pole[2][i];
 
                 if (tempSumX == 3 || tempSumX == -3) {
-                    //System.out.println("Выигрышь по горизонтали, " + (i + 1) + " строка. " + ((tempSumX == 3) ? "круги" : "крестики"));
                     victory[0] = i;
                     victory[1] = 0;
                     victory[2] = tempSumX;
@@ -161,7 +162,6 @@ public class Game {
                 }
 
                 if (tempSumY == 3 || tempSumY == -3) {
-                    //System.out.println("Выигрышь по вертикали, " + (i + 1) + " столбец. " + ((tempSumY == 3) ? "круги" : "крестики"));
                     victory[0] = i;
                     victory[1] = 1;
                     victory[2] = tempSumY;
@@ -172,14 +172,12 @@ public class Game {
                 int diagonalYX = pole[2][0] + pole[1][1] + pole[0][2];
 
                 if (diagonalXY == 3 || diagonalXY == -3) {
-                    //System.out.println("Выигрышь по вертикали, " + (i + 1) + " столбец. " + ((tempSumY == 3) ? "круги" : "крестики"));
                     victory[0] = i;
                     victory[1] = 2;
                     victory[2] = diagonalXY;
                     break;
                 }
                 if (diagonalYX == 3 || diagonalYX == -3) {
-                    //System.out.println("Выигрышь по вертикали, " + (i + 1) + " столбец. " + ((tempSumY == 3) ? "круги" : "крестики"));
                     victory[0] = i;
                     victory[1] = 3;
                     victory[2] = diagonalYX;
@@ -189,6 +187,14 @@ public class Game {
             }
         }
 
+        /**
+         * Метод определяет по координатам ячейку в которой необходимо
+         * нарисовать объект
+         *
+         * @param param - принимает значение координат мыши
+         * @return возвращает значение колонки илии строки. Нумерация начинается
+         * с 0.
+         */
         private int identifyCell(int param) {
             if (param < 200) {
                 param = 0;
@@ -202,6 +208,15 @@ public class Game {
             return param;
         }
 
+        /**
+         * Метод принимает значение номер колоки или строки игрового поля и
+         * возвращает соответствующие им координаты для рисования объекта
+         *
+         * @param param принимает значение номер колонки или строки. Нумерация
+         * начинается с 0
+         * @return возвращает значение в пикселях для каждой колонки или строки
+         * игрового поля
+         */
         private int identifyCellbyNumber(int param) {
 
             switch (param) {
@@ -222,6 +237,8 @@ public class Game {
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
 
+            //определяется положение нолики или крестика в зависимости 
+            //от нажатия кноки мыши
             for (int i = 0; i <= pole.length - 1; i++) {
                 for (int j = 0; j <= pole[i].length - 1; j++) {
 
@@ -245,6 +262,7 @@ public class Game {
 
             }
 
+            //блок рисует сетку на поле
             g.setColor(Color.BLUE);
             g.drawLine(200, 0, 200, 600);
             g.drawLine(400, 0, 400, 600);
@@ -255,27 +273,22 @@ public class Game {
             g.setColor(Color.MAGENTA);
             if (victory[0] > -1) {
                 Graphics2D g2d = (Graphics2D) g;
-                g2d.setStroke(new BasicStroke(5));
+                g2d.setStroke(new BasicStroke(5)); // устанавливаем толщину линии в 5 пикселей
                 int param = identifyCellbyNumber(victory[0]);
-                //System.out.println(param);
-                if (victory[1] == 1) {
 
-                    // устанавливаем толщину линии в 5 пикселей
-                    //g2d.drawLine(10, 10, 200, 10); // рисуем линию
+                if (victory[1] == 1) { //выигрышь по вертикали
                     g2d.drawLine(param, 50, param, 550);
-
-                } else if (victory[1] == 0) {
+                } else if (victory[1] == 0) { //выигрышь по горизонтали
                     g2d.drawLine(50, param, 550, param);
-                } else if (victory[1] == 2) {
+                } else if (victory[1] == 2) { //выигрышь по диагонали слева направа
                     g2d.drawLine(50, 50, 550, 550);
-                } else if (victory[1] == 3) {
+                } else if (victory[1] == 3) { //выигрышь по диагонали справа налево
                     g2d.drawLine(550, 50, 50, 550);
                 }
-                
 
             }
 
-            lblText.setText("11223");
+            lblText.setText("Текущий ход: " + ((player == 1) ? "нолики" : "крестики"));
         }
 
     }
@@ -286,30 +299,29 @@ public class Game {
 
     }
 
-    public static class MouseListener extends MouseAdapter {
-
-        private int mouseX;
-        private int mouseY;
-
-//        @Override
-//        public void mousePressed(MouseEvent e) {
-//            int button = e.getButton();
-//            int x = e.getX();
-//            int y = e.getY();
-//        }
-//        @Override
-//        public void mouseMoved(MouseEvent e) {
-//            mouseX = e.getX();
-//            mouseY  = e.getY();
-//        }
+//    public static class MouseListener extends MouseAdapter {
 //
-//        public int getMouseX() {
-//            return mouseX;
-//        }
+//        private int mouseX;
+//        private int mouseY;
 //
-//        public int getMouseY() {
-//            return mouseY;
-//        }
-    }
-
+////        @Override
+////        public void mousePressed(MouseEvent e) {
+////            int button = e.getButton();
+////            int x = e.getX();
+////            int y = e.getY();
+////        }
+////        @Override
+////        public void mouseMoved(MouseEvent e) {
+////            mouseX = e.getX();
+////            mouseY  = e.getY();
+////        }
+////
+////        public int getMouseX() {
+////            return mouseX;
+////        }
+////
+////        public int getMouseY() {
+////            return mouseY;
+////        }
+//    }
 }
